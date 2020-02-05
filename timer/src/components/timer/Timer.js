77c1df80180aaa1673548b1d94580ae3fb29ca02
Kyriden <matteo.clavel2@gmail.com>
2020-02-05 08:36:33 +0100

    secondCommit

--- timer/src/components/timer/Timer.js
+++ timer/src/components/timer/Timer.js
@@ -4,21 +4,27 @@ class Timer extends React.Component {
 
   constructor(props) {
     super(props);
-    // ...
+    this.state = { seconds : 0 };
   }
 
   componentDidMount() {
-    // ...
+    this.interval = setInterval(() => this.tick(), 1000);
   }
 
   componentWillUnmount() {
-    // ...
+    clearInterval(this.interval);
   }
-  
-  render () {
+
+  tick() {
+    this.setState(state => ({
+      seconds: state.seconds + 1
+    }));
+  }
+
+  render() {
     return (
       <div data-testid="timer">
-        {this.state.counter}
+        Il s'est écoulé {this.state.seconds} secondes depuis votre arrivée sur la page.
       </div>
     );
   }
